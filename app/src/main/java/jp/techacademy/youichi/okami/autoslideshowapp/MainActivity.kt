505894204dc12b479644f1216e1be374fd3e782e
okami-ap <y.okami@alphapolis.co.jp>
2024-03-12 09:12:03 +0900

    クリックリスナー設定

--- app/src/main/java/jp/techacademy/youichi/okami/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/youichi/okami/autoslideshowapp/MainActivity.kt
@@ -45,6 +45,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val button1 = findViewById<Button>(R.id.button1)
         button1.setOnClickListener{
             Log.d("UI_PARTS", "ボタンをタップしました")
+
+            // 画像の情報を取得する
+            val resolver = contentResolver
+            val cursor = resolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                null, // 項目（null = 全項目）
+                null, // フィルタ条件（null = フィルタなし）
+                null, // フィルタ用パラメータ
+                null // ソート (nullソートなし）
+            )
+            if (cursor!!.moveToFirst()) {
+                if (cursor.moveToNext()) {
+                    // indexからIDを取得し、そのIDから画像のURIを取得する
+                    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor.getLong(fieldIndex)
+                    val imageUri =
+                        ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                    binding.imageView.setImageURI(imageUri)
+                }
+            }
+            cursor.close()
         }
 
         // 戻る
