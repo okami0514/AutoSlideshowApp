10f0f49258ff6f5dfec219b41ea184b2504ec639
okami-ap <y.okami@alphapolis.co.jp>
2024-03-12 14:26:36 +0900

    クリックリスナー設定

--- app/src/main/java/jp/techacademy/youichi/okami/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/youichi/okami/autoslideshowapp/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.techacademy.youichi.okami.autoslideshowapp
 
 import android.content.ContentUris
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     private val PERMISSIONS_REQUEST_CODE = 100
+
+    private var cursor: Cursor? = null
 
     // APIレベルによって許可が必要なパーミッションを切り替える
     private val readImagesPermission =
@@ -46,27 +49,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button1.setOnClickListener{
             Log.d("UI_PARTS", "ボタンをタップしました")
 
-            // 画像の情報を取得する
-            val resolver = contentResolver
-            val cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-                null, // 項目（null = 全項目）
-                null, // フィルタ条件（null = フィルタなし）
-                null, // フィルタ用パラメータ
-                null // ソート (nullソートなし）
-            )
             if (cursor!!.moveToFirst()) {
-                if (cursor.moveToNext()) {
+                if (cursor!!.moveToNext()) {
                     // indexからIDを取得し、そのIDから画像のURIを取得する
-                    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                    val id = cursor.getLong(fieldIndex)
+                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor!!.getLong(fieldIndex)
                     val imageUri =
                         ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
                     binding.imageView.setImageURI(imageUri)
                 }
             }
-            cursor.close()
+            cursor!!.close()
         }
 
         // 戻る
@@ -103,7 +97,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun getContentsInfo() {
         // 画像の情報を取得する
         val resolver = contentResolver
-        val cursor = resolver.query(
+         cursor = resolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
             null, // 項目（null = 全項目）
             null, // フィルタ条件（null = フィルタなし）
@@ -113,13 +107,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         if (cursor!!.moveToFirst()) {
             // indexからIDを取得し、そのIDから画像のURIを取得する
-            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            val id = cursor.getLong(fieldIndex)
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
             val imageUri =
                 ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
             binding.imageView.setImageURI(imageUri)
         }
-        cursor.close()
     }
 }
